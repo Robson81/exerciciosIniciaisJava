a812b053fa65328337f5874c7bbd19cd5585e0a3
Robson Barbosa <robson.barbosa12@gmail.com>
2023-05-22 20:06:26 -0300

    primeiros exercicios do curso de java

---
+++ soma.java
@@ -0,0 +1,40 @@
+package ExerciciosParaIniciantes;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class soma {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("This program will sum some values ​​until you want to exit\n\n");
+
+		char ask;
+		do {
+
+			System.out.println("Prime two number do calculate");
+			System.out.print("1st: ");
+			int a = sc.nextInt();
+			System.out.print("2st: ");
+			int b = sc.nextInt();
+
+			int sum = a + b;
+
+			System.out.printf("The sum of %d and %d is: %d\n", a, b, sum);
+			System.out.print("Wish sum others values? Prime y to Yes or other to exit: ");
+
+			ask = sc.next().charAt(0);
+			System.out.println("\n");
+
+		} while (ask == 'y' || ask == 'Y');
+
+		System.out.println("Terminated Program!");
+
+		sc.close();
+	}
+
+}
